bb739c263a59bcb71388f4d75c11afab9a1cb77e
rudrabiloriya21 <rudrabiloriya21@gmail.com>
2026-03-17 22:51:04 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="mCyjVKy5Mcl0qzLW--dh2nD4OuyKDCIlGDzvosCxrrM" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
